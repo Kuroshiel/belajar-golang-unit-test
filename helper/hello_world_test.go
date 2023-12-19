5c2c468e070f7c680d81938f9eff67be86c0c05d
Kuroshiel <alifianfikry@gmail.com>
2023-12-19 16:39:14 +0700

    Learn TestSkip

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,21 +2,31 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestHelloWorldAssert(t *testing.T) {
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "linux" {
+		t.Skip("Can not run on Linux os")
+	}
+
 	result := HelloWorld("Eko")
-	assert.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
-	fmt.Println("TestHelloWorld with Assert Done")
+	require.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
 }
 
 func TestHelloWorldRequire(t *testing.T) {
 	result := HelloWorld("Eko")
 	require.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	fmt.Println("TestHelloWorld with Assert Done")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Eko")
+	assert.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
 	fmt.Println("TestHelloWorld with Assert Done")
 }
 
