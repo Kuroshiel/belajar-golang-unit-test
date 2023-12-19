5c5fe7f3af2330d52e79a3dc7702dc93bc61d70c
Kuroshiel <alifianfikry@gmail.com>
2023-12-19 16:58:07 +0700

    Learn SubTest

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestSubTest(t *testing.T) {
+	t.Run("Eko", func(t *testing.T) {
+		result := HelloWorld("Eko")
+		require.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	})
+
+	t.Run("Kurniawan", func(t *testing.T) {
+		result := HelloWorld("Kurniawan")
+		require.Equal(t, "Hello Kurniawan", result, "Result must be 'Hello Kurniawan'")
+	})
+}
+
 func TestMain(m *testing.M) {
 	// before
 	fmt.Println("Before Unit Test")
